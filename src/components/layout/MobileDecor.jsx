b0f1067242a7bb1206c96fb76fc4eb2642cfdc04
Nicolas <nico_kol@hotmail.com>
2026-01-20 10:04:31 -0300

    bublers +1

--- src/components/layout/MobileDecor.jsx
+++ src/components/layout/MobileDecor.jsx
@@ -3,21 +3,27 @@ import styles from "./MobileDecor.module.css";
 export default function MobileDecor() {
     return (
         <div className={styles.stage} aria-hidden="true">
-        {/* Arriba */}
-        <div className={`${styles.bubble} ${styles.orange} ${styles.o1}`} />
-        <div className={`${styles.bubble} ${styles.violet} ${styles.v1}`} />
-        <div className={`${styles.bubble} ${styles.blue} ${styles.b1}`} />
+            {/* Arriba */}
+            <div className={`${styles.bubble} ${styles.orange} ${styles.o1}`} />
+            <div className={`${styles.bubble} ${styles.violet} ${styles.v1}`} />
+            <div className={`${styles.bubble} ${styles.blue} ${styles.b1}`} />
 
-        {/* Medio */}
-        <div className={`${styles.bubble} ${styles.orange} ${styles.o2}`} />
-        <div className={`${styles.bubble} ${styles.violet} ${styles.v2}`} />
-        <div className={`${styles.bubble} ${styles.blue} ${styles.b2}`} />
+            {/* Medio */}
+            <div className={`${styles.bubble} ${styles.orange} ${styles.o2}`} />
+            <div className={`${styles.bubble} ${styles.violet} ${styles.v2}`} />
+            <div className={`${styles.bubble} ${styles.blue} ${styles.b2}`} />
 
-        {/* Abajo */}
-        <div className={`${styles.bubble} ${styles.orange} ${styles.o3}`} />
-        <div className={`${styles.bubble} ${styles.violet} ${styles.v3}`} />
-        <div className={`${styles.bubble} ${styles.blue} ${styles.b3}`} />
+            {/* Medio-bajo */}
+            <div className={`${styles.bubble} ${styles.orange} ${styles.o3}`} />
+            <div className={`${styles.bubble} ${styles.violet} ${styles.v3}`} />
+            <div className={`${styles.bubble} ${styles.blue} ${styles.b3}`} />
+
+            {/* Abajo */}
+            <div className={`${styles.bubble} ${styles.orange} ${styles.o4}`} />
+            <div className={`${styles.bubble} ${styles.violet} ${styles.v4}`} />
+            <div className={`${styles.bubble} ${styles.blue} ${styles.b4}`} />
         </div>
     );
 }
+
 
